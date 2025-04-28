87bf6cec9f0da38b8aadb163553842d49e4bf787
Sandschool <trantruong7826@gmail.com>
2025-04-28 13:57:14 +0700

    tronll

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/mvcdemo/',
+    'baseURL' => 'http://localhost/mvcdemo/',
+    'assets' => '/mvcdemo/assets',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
